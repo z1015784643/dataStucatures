5ca7f725828a988135072d2ff6ffbf08a913561d
zhangqi <1015784643@qq.com>
2020-05-15 20:15:40 +0800

    归并排序，快速排序

--- DS5_SortAndSerach/3.sort.py
+++ DS5_SortAndSerach/3.sort.py
@@ -62,7 +62,6 @@ print(nums)
 第二次遍历： n - 2
 次
 
-
 # 实现冒泡排序
 # def bubbleSort(alist):
 #     for passnum in range(len(alist)-1,0,-1):
@@ -133,17 +132,132 @@ print(alist)
     第一步 [26,54]    [93,17,77,31,44,55,20],取出26比较，然后插入
     第二步 [26,54,93]    [17,77,31,44,55,20],取出93比较，然后插入
 '''
-def insertSort(alist):
-    for index in range(1,len(alist)):
-        currentValue = alist[index]
-        position = index
-        while position > 0 and alist[position-1] > currentValue:
-            alist[position] = alist[position-1]
-            position = position-1
+# def insertSort(alist):
+#     for index in range(1,len(alist)):
+#         currentValue = alist[index]
+#         position = index
+#         while position > 0 and alist[position-1] > currentValue:
+#             alist[position] = alist[position-1]
+#             position = position-1
+#             alist[position] = currentValue
+# alist = [54,26,93,17,77,31,44,55,20]
+# insertSort(alist)
+# print(alist)
+
+
+'''
+力扣第147题
+四、希尔排序（缩小间隔排序）：以插入排序为基础，将原来要排序的列表划分成一列子列表，再对每一个子列表执行插入操作，从而实现对插入排序性能的改进。
+    关键：划分子列表的特定方法
+    alist = [54,26,93,17,77,31,44,55,20]
+    1.以3为间隔来划分，分成3个子列表.将列表以3为间隔，来划分，每隔3个数字取一个值
+    [54,17,44]    [17,26,93,44,77,31,54,55,20]
+    [26,77,55]    [17,26,93,44,55,31,54,77,20]
+    [93,31,20]    [17,26,20,44,55,31,54,77,93]
+    
+    在进行标准插入排序：[17],[26,20,44,55,31,54,77,93]
+    优化了对比和移动的次数
+    
+    2.以2为间隔，划分成5个子列表
+    [54,93]  
+    [17,31]  
+    [44,20]
+    [26,77]
+    [31,55]
+    
+
+def shellSort(alist):
+    pass
+
+def gapInsertionSort(alist,start,gap):
+    for i in range(0,len(alist),gap):
+        currentValue = alist[i]
+        position = i
+
+        if position >= gap and alist[position-gap] > currentValue:
+            alist[position] = alist[position-gap]
+            position = position - gap
             alist[position] = currentValue
+
 alist = [54,26,93,17,77,31,44,55,20]
-insertSort(alist)
+for i in range(len(alist)//3):
+    gapInsertionSort(alist,i,3)
 print(alist)
+'''
+
+'''
+    五、归并排序
+    具体做法：递归算法，不断地将列表拆分成一半，
+    如果列表为空或者只有一个元素，那么根据定义，它就排好了。
+    alist = [54,26,93,17,77,31,44,55,20]
+    第一次拆分[54,26,93,17]            [77,31,44,55,20]
+    第二次拆分[54,26] [93,17]          [77,31] [44,55,20]
+    第三次拆分[54][26][93][17]         [77][31][44][55,20]
+    第四次拆分                                   [55][20]
+    
+    
+    [54][26]  [93][17]      [77][31] [44][55] [20]
+1.    [26,54]  [17,93]        [31,77]     [20,55].
+2.     [17,26,54,93]                  [20,44,55]
+3.                             [20,31,44,55,77]
+4.              [17,20,26,31,44,54,55,77,93]
+'''
+def mergeSort(alist):
+    if len(alist) > 1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i =0
+        j = 0
+        k = 0
+        while i <len(righthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i=i+1
+            else:
+                alist[k] = righthalf[j]
+                j = j +1
+            k = k + 1
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+mergeSort(alist)
+print(alist)
+
+
+
+'''
+六、快速排序
+    选择一个值作为枢纽值，一般就是列表第一项，枢纽值作为帮助拆分的标准
+
+    alist = [54,26,93,17,77,31,44,55,20]
+            [17,20,26,31,44,54,55,77,93]
+    54始终在原列表中31的位置上，这个位置叫做拆分点
+    
+    54  左标记[26,93,17,77,31,44,55,20]  右标记
+    首先增加左标记，直到找到一个大于枢纽值的值停止，然后递减右标记，直到找到一个小于枢纽值的值停止，交换这两项。
+
+'''
+
+
+
+
+
+
+
+
+
 
 
 
